9b8dc83eab7b86844be55b7fcf01eb79017d9bfe
dsilver829@gmail.com <dsilver829@gmail.com>
2016-10-17 22:00:30 +0000

    adding convnet

--- tf_feedforward.py
+++ tf_feedforward.py
@@ -17,41 +17,30 @@ X_train = X_train.astype('float32')
 X_val = X_val.astype('float32')
 X_test = X_test.astype('float32')
 
-batch_size = 64
-
 # 0-255 -> 0-1
 X_train /= 255
 X_val /= 255
 X_test /= 255
 
+batch_size = 64
 # number of traffic signs
 n_classes = 43
 epochs = 10
 input_shape = X_train.shape[1:]
 
-feature_shape = (32, 32, 3)
 feature_size = 32*32*3
 
-# CONV1
-features = tf.placeholder(tf.float32, (batch_size,) + feature_shape, name="features")
-filters = tf.Variable(tf.truncated_normal([3,3,3,32]))
-conv1 = tf.nn.conv2d(features, filters, strides=(1,2,2,1), padding="VALID")
-relu = tf.nn.relu(conv1)
-relu_shape = relu.get_shape().as_list()
-reshape = tf.reshape(relu, [relu_shape[0], relu_shape[1]*relu_shape[2]*relu_shape[3]])
-
-# FC2
-feature_size = feature_shape[0] // 3 * feature_shape[1] // 3 * 32
+# tf.nn.conv2d(input, filter, strides, padding)
 W1 = tf.Variable(tf.random_normal((feature_size, 100), mean=0, stddev=0.01) , name="W1")
-b1 = tf.Variable(tf.zeros((100)) , name="b1")
-
-# FC3
 W2 = tf.Variable(tf.random_normal((100, n_classes), mean=0, stddev=0.01) , name="W2")
+
+b1 = tf.Variable(tf.zeros((100)) , name="b1")
 b2 = tf.Variable(tf.zeros((n_classes)) , name="b2")
 
+features = tf.placeholder(tf.float32, (None, feature_size), name="features")
 labels = tf.placeholder(tf.int64, (None), name="labels")
 
-out1 = tf.matmul(reshape, W1) + b1
+out1 = tf.matmul(features, W1) + b1
 out1 = tf.nn.relu(out1)
 logits = tf.matmul(out1, W2) + b2
 
@@ -69,19 +58,18 @@ with tf.Session() as sess:
 			end = offset + batch_size
 			batch_X = X_train[offset:end]
 			batch_y = y_train[offset:end]	
-
-			if batch_X.shape[0] != batch_size:
-				continue
+			bs = batch_X.shape[0]
+			batch_X = batch_X.reshape(bs, feature_size)
 
 			l, _ = sess.run([loss, train_op], feed_dict={features: batch_X, labels: batch_y})
 
-		val_l, val_acc = sess.run([loss, accuracy], feed_dict={features: X_val, labels: y_val})
+		val_l, val_acc = sess.run([loss, accuracy], feed_dict={features: X_val.reshape(X_val.shape[0], feature_size), labels: y_val})
 		print("Validation	Loss =", val_l)
 		print("Validation	Accuracy =", val_acc)
 
 	
-	#test_l, test_acc = sess.run([loss, accuracy], feed_dict={features: X_test, labels: y_test})
-	#print("Testing	Loss =", test_l)
-	#print("Testing	Accuracy =", test_acc)
+	test_l, test_acc = sess.run([loss, accuracy], feed_dict={features: X_test.reshape(X_test.shape[0], feature_size), labels: y_test})
+	print("Testing	Loss =", test_l)
+	print("Testing	Accuracy =", test_acc)
 
 
