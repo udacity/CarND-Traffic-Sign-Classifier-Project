2fb3e4b813743bc18e672c11f7f3584a2430515a
Mihael Vaganov <asterx@yandex-team.ru>
2017-04-27 16:15:58 +0300

    Implementation

--- utils/image.py
+++ utils/image.py
@@ -1,16 +1,22 @@
 # coding=utf-8
 import glob
+import operator
+from csv import reader
 from numpy import array, float32, random, uint8, zeros
 from cv2 import \
+    COLOR_BGR2GRAY, \
     IMREAD_COLOR, \
+    INTER_CUBIC, \
     GaussianBlur, \
     add, \
     addWeighted, \
+    cvtColor, \
     equalizeHist, \
     getRotationMatrix2D, \
     imread, \
     merge, \
     multiply, \
+    resize, \
     split, \
     warpAffine
 
@@ -23,12 +29,17 @@ def eq_Hist(img_in):
 
 def rotate(img_in):
     c_x, c_y = int(img_in.shape[0]/2), int(img_in.shape[1]/2)
-    ang = 30.0 * random.rand() - 15
+    ang = random.choice((operator.add, operator.sub))(45 * random.rand(), 25 * random.rand())
     Mat = getRotationMatrix2D((c_x, c_y), ang, 1.0)
     return warpAffine(img_in, Mat, img_in.shape[:2])
 
+def zoom_in(img_in, mar=0, img_size=32):
+    c_x,c_y, sh = int(img_in.shape[0]/2), int(img_in.shape[1]/2), int(img_size/2-mar)
+    img = img_in[(c_x-sh):(c_x+sh),(c_y-sh):(c_y+sh)]
+    return resize(img, (img_size, img_size), interpolation = INTER_CUBIC)
+
 def sharpen(img_in):
-    gb = GaussianBlur(img_in, (5,5), 15.0)
+    gb = GaussianBlur(img_in, (3, 3), 3.0)
     return addWeighted(img_in, 2, gb, -1, 0)
 
 def lin(img_in, s=1.0, m=0.0):
@@ -39,14 +50,16 @@ def contr(img_in, s=1.0):
     return lin(img_in, s, 127.0*(1.0-s))
 
 def transform(img_in):
-    img = sharpen(img_in)
-    img = contr(img, 1.5)
+    img = zoom_in(img_in, 3)
+    img = sharpen(img)
     return eq_Hist(img)
 
 def augment(img_in):
-    img = contr(img_in, 1.8 * random.rand() + 0.2)
-    img = rotate(img)
+    img = rotate(img_in)
     return transform(img)
+
+def gray(img_in):
+    return cvtColor(img_in, COLOR_BGR2GRAY)
 
 # Generating new images from existing ones using transformations
 def generate_images(X_train_sub, y_train_sub, X_val, y_val, n = 1):
@@ -72,11 +85,19 @@ def generate_images(X_train_sub, y_train_sub, X_val, y_val, n = 1):
         array(y_train_aug, dtype = float32), \
         array(X_val_prep, dtype = float32)
 
-def load_test_images(path):
+def load_test_images(path, f_name, need_gray = False):
     test_img = uint8(zeros((8, 32, 32, 3)))
     for i, imp in enumerate([p for p in glob.glob(path)]):
         img = imread(imp, IMREAD_COLOR)
         b, g, r = split(img)
         img = merge([r, g, b])
-        test_img[i] = img
-    return test_img
+        if need_gray:
+            img = gray(img)
+        test_img[i] = transform(img)
+
+    with open(f_name, 'rt') as dfile:
+        r = reader(dfile, delimiter=',')
+        test_sign_class = [ row[0] for row in r ][1:]
+        test_sign_class = [ int(i) for i in test_sign_class ]
+
+    return test_img, test_sign_class
--- utils/plot.py
+++ utils/plot.py
@@ -1,27 +1,48 @@
 # coding=utf-8
 from math import ceil
-from numpy import arange
+from csv import reader
+from numpy import arange, array, uint32, linspace, all
 import matplotlib.pyplot as plt
+from scipy.interpolate import spline
 
-def plot_train_images(X_train, y_train, n_classes):
+def plot_train_images(X_train, y_train, n_classes, is_gray = False):
+    cmap = 'gray' if is_gray else None
     plt.figure(figsize=(20, 15))
     for i in range(0, n_classes):
         plt.subplot(7, 7, i+1)
         x_selected = X_train[y_train == i]
-        plt.imshow(x_selected[0, :, :, :])
+        plt.imshow(x_selected[0, :, :, :], cmap=cmap)
         plt.title(i)
         plt.axis('off')
+    plt.savefig('./out/train_images.png')
     plt.show()
 
-def plot_test_images(test_img, signs_class = None, signs_classes = None, per_line = 4):
+def plot_distribution(X_train, y_train, n_classes):
+    samples_count = []
+    for i in range(0, n_classes):
+        x_selected = X_train[y_train == i]
+        samples_count.append(len(x_selected))
+    plt.figure(figsize=(12, 4))
+    plt.bar(range(0, n_classes), samples_count)
+    plt.title('Distribution')
+    plt.xlabel('Classes')
+    plt.ylabel('Images')
+    plt.savefig('./out/distribution.png')
+    plt.show()
+
+
+def plot_test_images(test_img, signs_class = None, signs_classes = None, per_line = 4, name = None, is_gray = False):
+    has_class = True if signs_class is not None and signs_classes is not None else False
+    cmap = 'gray' if is_gray else None
     images_count = len(test_img)
     lines = ceil(images_count/per_line)
     plt.figure(figsize=(12, 8))
     for i in range(images_count):
         plt.subplot(lines, per_line, i+1)
-        plt.imshow(test_img[i])
-        plt.title(signs_class[signs_classes[i]] if signs_class and signs_classes else i + 1)
+        plt.imshow(test_img[i], cmap=cmap)
+        plt.title(signs_class[signs_classes[i]] if has_class else i + 1)
         plt.axis('off')
+    plt.savefig('./out/test_images' + ('_' + name if name is not None else '') + '.png')
     plt.show()
 
 def plot_probabilities(test_img, top5, signs_class):
@@ -36,14 +57,25 @@ def plot_probabilities(test_img, top5, signs_class):
         labs = [signs_class[j] for j in top5.indices[i]]
         plt.yticks(arange(1, 6, 1), labs)
     plt.show()
+    plt.savefig('./out/probabilities.png')
 
-def plot_learing_progress(f_acc):
-    with open(f_acc, 'rt') as accfile:
-        accreader = csv.reader(accfile, delimiter=',')
-        acc = [row[1] for row in accreader][1:]
+def plot_learing_progress(f_name):
+    with open(f_name, 'rt') as dfile:
+        r = reader(dfile, delimiter=',')
+        acc = [ row[1] for row in r ][1:]
+        dfile.seek(0)
+        loss = [ row[2] for row in r ][1:]
         acc.insert(0, 0)
-        plt.plot(list(range(0, len(acc))), acc, label='Accuracy on training set')
-        plt.xlabel('Epoch')
-        plt.ylabel('Accuracy')
-        plt.legend()
-        plt.show()
+        for item in ((acc, 'Accuracy'), (loss, 'Loss')):
+            x = array(range(0, len(item[0])))
+            y = [ float(i) for i in item[0] ]
+            xnew = linspace(x.min(), x.max(), 100)
+            plt.plot(xnew, spline(x, y, xnew), label=item[1] + ' on test set')
+            plt.xlabel('Epoch')
+            plt.ylabel(item[1])
+            plt.legend()
+            plt.show()
+            plt.savefig('./out/learing_progress_' + item[1].lower() + '.png')
+
+if __name__ == '__main__':
+    plot_learing_progress('../logs/acc-loss.csv')
