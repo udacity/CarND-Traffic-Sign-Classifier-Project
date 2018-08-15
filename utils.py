f71444328c27e9b7a23b05baaa464619041f65de
smendelowitz <smendelowitz@tower-sec.com>
2018-08-15 15:58:11 +0000

    full accuracy

--- utils.py
+++ utils.py
@@ -4,5 +4,5 @@ def rescue_code(function):
     import inspect
     get_ipython().set_next_input("".join(inspect.getsourcelines(function)[0]))
 
-def setup_figure(x_pics, y_pics, w, h, n=10):
-    return plt.subplots(y_pics, x_pics, squeeze=True, sharex=True, sharey=True, figsize=(x_pics * n, y_pics * n))#h / w * n))        
+def setup_figure(x_pics, y_pics, w, h, n=10, squeeze=True, sharex=True, sharey=True):
+    return plt.subplots(y_pics, x_pics, squeeze=squeeze, sharex=sharex, sharey=sharey, figsize=(x_pics * n, y_pics * n))#h / w * n))        
